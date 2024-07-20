87227a902d3762fafe166c8e9aee7cdc9fc7b15f
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-07-20 10:17:12 +0000

    operators in javascript

---
+++ 01_Basics/004_operators.js
@@ -0,0 +1,69 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2 ki power 3
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+let str1 = "Aryan"
+let str2 = " Mehta"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// Complex Situations
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 3); */
+
+
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+
+
+// console.log("2" > 1);  //here javascript will automatically convert string into number
+// console.log("02" > 1);
+
+
+
+/* This type of conversions always makes us confused
+
+console.log(null > 0);     // The reason is that an equality check (==) and comparisons (>, <, >=, <=) work differently.
+console.log(null == 0);    // Comparisons convert null into a number, treating it as 0.
+console.log(null >= 0);    // That's why null >= 0 is true and null > 0 is false.
+
+console.log(undefined > 0);   // false
+console.log(undefined == 0);  // false
+console.log(undefined >= 0);  // false
+
+*/
+
+
+
+/* strict equality operator (===)*/
+//console.log("2" === 2);  // This means that both the value and the type of the operands must be the same for the comparison to return true
+
+
+
+/* strict inequality operator (!==) */
+//console.log(5 !== "5");  // It compares two values to determine whether they are not equal in both value and type.
+
+
+
+
+// To learn more about Operators:- https://www.w3schools.com/js/js_operators.asp
