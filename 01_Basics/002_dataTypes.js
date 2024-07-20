344d28a14158018a748dab8e25a5c55d16c14ef2
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-07-20 07:45:08 +0000

    data types in javascript

---
+++ 01_Basics/002_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, and it will work only on browser
+
+console.log(3 
+    + 
+    3)          // never write like this. Code readability should be High
+
+console.log("Aryan") // It's perfect
+
+
+
+let Name = "Aryan" // string
+let age = 18 // number
+let isLoggedIn = true // boolean
+
+
+/* Primitive Data types in JS
+
+Number => 2^53
+String => "Aryan"
+Boolean => true/false
+Undefined => value which is not defined
+Null => standalone value (empty)
+BigInt => Big numbers 
+Symbol => unique
+
+*/
+
+/* Non-Primitive Data type => object */
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
