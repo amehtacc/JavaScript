329671a6c81ad15d74aaa882841d63699921b829
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-07-25 18:29:35 +0000

    functions in javascript

--- 03_Basics/001_functions.js
+++ 03_Basics/001_functions.js
@@ -42,4 +42,30 @@ function loginUserMessage(username){
 function calculateCartPrice(val1, val2, ...num1){   //... is called rest operator and also spread operator. But it completely depends on its use cases that when it act like rest operator or spread operator
     return num1
 }
-console.log(calculateCartPrice(200, 500, 300, 1500))
+// console.log(calculateCartPrice(200, 500, 300, 1500)) // rest operator will put all values into an array and return the array
+
+
+const user = {
+    username: "aryan",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user) // it is not necessary that we pass the object by already creating it 
+handleObject({
+    username: "tony",   // we can pass it directly in the calling function
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 300, 700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 300, 700]));   // like object, here also we can pass the Array directly in the calling function
