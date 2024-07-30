905f7cdfb112e31f4557fa7da017d305b97a1c9d
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-07-30 13:06:00 +0000

    reduce method of array in javascript

---
+++ 05_iterations/008_reduce_method.js
@@ -0,0 +1,42 @@
+/* 
+-> The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. 
+The final result of running the reducer across all elements of the array is a single value.
+
+-> The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. 
+Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+*/
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {            // acc = accumulator, currval = current value, 0 = initial value
+//     console.log(`acc: ${acc} and currval: ${currval}`);            // here accumulator(previous value) take the initial value and the sum of accumulator & current value saved again in accumulator
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
