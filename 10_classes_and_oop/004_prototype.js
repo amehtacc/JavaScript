89b1b8d0e05cba94828189323aff8e82964b7661
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-08-09 17:58:55 +0000

    prototypes in javascript

---
+++ 10_classes_and_oop/004_prototype.js
@@ -0,0 +1,67 @@
+// let myName = "aryan     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.aryan = function(){
+    console.log(`aryan is present in all objects`);
+}
+
+Array.prototype.heyaryan = function(){
+    console.log(`Aryan says hello`);
+}
+
+// heroPower.aryan()
+// myHeros.aryan()
+// myHeros.heyaryan()
+// heroPower.heyaryan()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"aryan".trueLength()
+"iceTea".trueLength()
