2e524abdbc5b599f02fc2305abbe1410362c1854
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-08-11 01:01:25 +0000

    getter and setter function in javascript

---
+++ 10_classes_and_oop/011_getter_setter.js
@@ -0,0 +1,78 @@
+/* Class based getter setter */
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}aryan`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const aryan = new User("a@aryan.ai", "abc")
+console.log(aryan.email);
+
+
+
+
+
+/* Function based getter setter */
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+console.log(chai.email);
+
+
+
+
+
+/* Object based getter setter */
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
